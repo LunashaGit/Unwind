877072f70cba1a5ce513a7f59e5fea443f6f33fd
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-04-21 00:02:13 +0200

    Add login & Data (Post, ProfilUser, Admin, Pro)

---
+++ src/assets/data/post.js
@@ -0,0 +1,42 @@
+export const Post = [
+    {
+        "id": "1",
+        "name": "Post1",
+        "description": "Psychiatre",
+        "star": "../../../public/images/star.png",
+        "starParagraphe": "4.5/5",
+        "image": "../../../public/images/1.jpg"
+    },
+    {
+        "id": "2",
+        "name": "Post2",
+        "description": "Profil Pro",
+        "star": "../../../public/images/star.png",
+        "starParagraphe": "4.5/5",
+        "image": "../../../public/images/2.jpg"
+    },
+    {
+        "id": "3",
+        "name": "Post3",
+        "description": "Profil Pro",
+        "star": "../../../public/images/star.png",
+        "starParagraphe": "4.5/5",
+        "image": "../../../public/images/3.jpg"
+    },
+    {
+        "id": "4",
+        "name": "Post4",
+        "description": "Profil Pro",
+        "star": "../../../public/images/star.png",
+        "starParagraphe": "4.5/5",
+        "image": "../../../public/images/4.jpg"
+    },
+    {
+        "id": "5",
+        "name": "Post5",
+        "description": "Profil Pro",
+        "star": "../../../public/images/star.png",
+        "starParagraphe": "4.5/5",
+        "image": "../../../public/images/5.jpg"
+    }
+]   
---
+++ src/assets/data/profilUser.js
@@ -0,0 +1,32 @@
+export const ProfilUser = [
+    {
+        "id": "1",
+        "name": "Kube Paul",
+        "description": "Psychiatre",
+        "image": "../../../public/images/1.jpg"
+    },
+    {
+        "id": "2",
+        "name": "Map Joseph",
+        "description": "Profil Pro",
+        "image": "../../../public/images/2.jpg"
+    },
+    {
+        "id": "3",
+        "name": "Pitch Vicky",
+        "description": "Profil Pro",
+        "image": "../../../public/images/3.jpg"
+    },
+    {
+        "id": "4",
+        "name": "Marshall Tanya",
+        "description": "Profil Pro",
+        "image": "../../../public/images/4.jpg"
+    },
+    {
+        "id": "5",
+        "name": "Art Pierre",
+        "description": "Profil Pro",
+        "image": "../../../public/images/5.jpg"
+    }
+]
