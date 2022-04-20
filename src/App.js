b0013b753e77769b194f55b0bd4232591bcb3506
SashBFa <bastienfajs@gmail.com>
2022-04-20 22:44:09 +0200

    react init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { AnimatePresence } from "framer-motion";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./assets/pages/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AnimatePresence>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AnimatePresence>
+    </BrowserRouter>
+  );
+};
+
+export default App;
