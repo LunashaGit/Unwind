c02c7b40fd707195d8e2340c6725cacffdbbaf87
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-04-21 11:47:58 +0200

    Post data update

--- src/assets/data/post.js
+++ src/assets/data/post.js
@@ -3,83 +3,155 @@ export const postData = [
         "id": "1",
         "name": "Post1",
         "description": "Psychiatre",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../public/images/ProfilPro/1.jpg"
+        "image": "../../public/images/ProfilPro/1.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
     {
         "id": "2",
         "name": "Post2",
         "description": "Profil Pro",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/2.jpg"
+        "image": "../../../public/images/2.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
     {
         "id": "3",
         "name": "Post3",
         "description": "Profil Pro",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/3.jpg"
+        "image": "../../../public/images/3.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
     {
         "id": "4",
         "name": "Post4",
         "description": "Profil Pro",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/4.jpg"
+        "image": "../../../public/images/4.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
     {
         "id": "5",
         "name": "Post5",
         "description": "Profil Pro",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/5.jpg"
+        "image": "../../../public/images/5.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
     {
         "id": "6",
         "name": "Post5",
         "description": "Profil Pro",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/5.jpg"
+        "image": "../../../public/images/5.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
     {
         "id": "7",
         "name": "Post5",
         "description": "Profil Pro",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/5.jpg"
+        "image": "../../../public/images/5.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
     {
         "id": "8",
         "name": "Post5",
         "description": "Profil Pro",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/5.jpg"
+        "image": "../../../public/images/5.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
 
     {
         "id": "9",
         "name": "Post5",
         "description": "Profil Pro",
+        "textDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed euismod, urna eu tincidunt tincidunt, nisi nisl aliquam eros, eget tincidunt nunc nisl euismod erat. Nulla facilisi. Sed" ,
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/5.jpg"
+        "image": "../../../public/images/5.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     },
     {
         "id": "10",
@@ -87,8 +159,15 @@ export const postData = [
         "description": "Profil Pro",
         "star": "../../../public/images/star.png",
         "starParagraphe": "4.5/5",
+        "city": "Bruxelles",
+        "notice": Math.floor(Math.random() * (25 - 10 +1 ) + 10),
         "match": Math.floor(Math.random() * (100 - 70 +1 ) + 70),
-        "image": "../../../public/images/5.jpg"
+        "image": "../../../public/images/5.jpg",
+        "tags": [
+            "psychiatre",
+            "psychologue",
+            "psychologie"
+        ]
     }
 
     
