b899d2d387e29cbf384da0e85290a86c4de789f6
SashBFa <bastienfajs@gmail.com>
2022-04-21 08:34:02 +0200

    react init 3

---
+++ src/assets/components/NavigationOnScroll.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavigationOnScroll = () => {
+  return (
+    <div style={{ position: "fixed" }}>
+      <h2>HEAAAAAAAH</h2>
+    </div>
+  );
+};
+
+export default NavigationOnScroll;
